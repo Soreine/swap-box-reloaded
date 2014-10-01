90781511341dca5df28c89284891c470785f2ee9
Soreine <soreine.plume@gmail.com>
2014-10-01 08:28:00 +0200

    Init functions

--- js/states/Play.js
+++ js/states/Play.js
@@ -19,6 +19,10 @@ SB2.Play.prototype.constructor = SB2.Play;
 // Members
 //------------------------------------------------------------------------------
 
+/** Controls of player 1 and 2 */
+SB2.Play.controls1;
+SB2.Play.controls2;
+
 /** This group contains all the solid and fixed platforms */
 SB2.Play.platforms;
 
@@ -52,105 +56,24 @@ SB2.Play.prototype.preload = function () {
 };
 
 SB2.Play.prototype.create = function () {
-    // Temporary variable to create ground objects
-    var ground,
-        // Temporary variable to create swap indicators
-        indic,
-        // Reference to the keyboard
-        kb,
-        // The two sets of Controls
-        control1,
-        control2,
-        // Some constants
-        HEIGHT = SB2.HEIGHT,
-        WIDTH = SB2.WIDTH,
-        INDIC_THICK = SB2.INDIC_THICK,
-        UNIT = SB2.UNIT;
-    
     this.state = this.RUNNING;
-
     //  We're going to be using physics, so enable the Arcade Physics system
     this.game.physics.startSystem(Phaser.Physics.ARCADE);
-
     // Initilize the background
     this.initBackground();
-
-    /** Adjust the size of the world */
+    /* Adjust the size of the world */
     this.game.world.setBounds(0, 0, SB2.WIDTH*5, SB2.HEIGHT);
-
     //  Initialize the platforms group
-    this.platforms = this.game.add.group(undefined, // No parent group
-			       'platforms', // Name for debug
-			       false, // Add directly to the stage
-			       true, // Enable body
-			       Phaser.Physics.ARCADE);
-
-    // Here we create the ground.
-    ground = this.platforms.create(0, HEIGHT - 2*UNIT, 'plain');
-    //  Scale it to fit the width of the game
-    ground.scale.setTo(WIDTH*10, 2*UNIT);
-    //  This stops it from falling away when you jump on it
-    ground.body.immovable = true;
-
-    ground = this.platforms.create(0, HEIGHT - 4*UNIT, 'plain');
-    //  Scale it to fit the width of the game
-    ground.scale.setTo(WIDTH/3, 2*UNIT);
-    //  This stops it from falling away when you jump on it
-    ground.body.immovable = true;
-
-    ground = this.platforms.create(WIDTH/2, HEIGHT - 7*UNIT, 'plain');
-    //  Scale it to fit the width of the game
-    ground.scale.setTo(WIDTH/2, 2*UNIT);
-    //  This stops it from falling away when you jump on it
-    ground.body.immovable = true;
-
+    this.initLevel();
     // Initialize controls
-    kb = this.game.input.keyboard;
-    control1 = new SB2.Controls(kb.addKey(Phaser.Keyboard.FIVE),
-			    null,
-			    kb.addKey(Phaser.Keyboard.Y),
-			    kb.addKey(Phaser.Keyboard.R));
-    control2 = new SB2.Controls(kb.addKey(Phaser.Keyboard.UP),
-			    null,
-			    kb.addKey(Phaser.Keyboard.RIGHT),
-			    kb.addKey(Phaser.Keyboard.LEFT));
-
-
-    // Initialize this player entity
-    this.cube1 = new SB2.Cube(this.game, WIDTH/4, HEIGHT/2, control1);
-    this.cube2 = new SB2.Cube(this.game, 3*WIDTH/4, HEIGHT/2, control2);
-
-    /** Initialize the cameraman */
+    this.initControls();
+    // Initialize the players entities
+    this.cube1 = new SB2.Cube(this.game, SB2.WIDTH/4, SB2.HEIGHT/2, this.controls1);
+    this.cube2 = new SB2.Cube(this.game, 3*SB2.WIDTH/4, SB2.HEIGHT/2, this.controls2);
+    /* Initialize the cameraman */
     // this.cameraman = new SB2.Cameraman(this.game.camera, this.game.time);
-
     // Initialize the swap indicators 
-    this.swapIndicators = this.game.add.group(undefined, 'indicators', true, 
-				    false); // No body
-    this.swapIndicators.alpha = 0;
-
-    // Create 4 bars assembling into a frame
-    // TOP
-    indic = this.swapIndicators.create(0, 0, 'plain');
-    indic.scale.setTo(WIDTH, INDIC_THICK);
-    // BOTTOM
-    indic = this.swapIndicators.create(0, HEIGHT - INDIC_THICK, 'plain');
-    indic.scale.setTo(WIDTH, INDIC_THICK);
-    // LEFT
-    indic = this.swapIndicators.create(WIDTH - INDIC_THICK, INDIC_THICK, 'plain');
-    indic.scale.setTo(INDIC_THICK, HEIGHT - 2*INDIC_THICK);
-    // RIGHT 
-    indic = this.swapIndicators.create(0, INDIC_THICK, 'plain');
-    indic.scale.setTo(INDIC_THICK, HEIGHT - 2*INDIC_THICK);
-
-    //Init the indicator tweener
-    this.swapTween = {primary: this.game.add.tween(this.swapIndicators),
-		      secondary: this.game.add.tween(this.swapIndicators)};
-    this.swapTween.primary.from({alpha:0}, SB2.INDIC_PERIOD/2);
-    this.swapTween.secondary.from({alpha:0}, SB2.INDIC_PERIOD/4);
-
-    // Init swap timer
-    this.swap = {timer:this.game.time.now,
-                 count:0};
+    this.initSwap();
 };
 
 SB2.Play.prototype.update = function () {
@@ -163,14 +86,14 @@ SB2.Play.prototype.update = function () {
         this.updateRunning();
         break;
     }
-}
+};
 
 /** Update function that pause the game */
 SB2.Play.prototype.updatePaused = function () {
     // Cubes shouldn't move
     this.cube1.body.velocity = {x: 0, y:0};
     this.cube2.body.velocity = {x: 0, y:0};
-}
+};
 
 SB2.Play.prototype.updateRunning = function () {
 
@@ -235,27 +158,6 @@ SB2.Play.prototype.handleSwap = function () {
     }
 };
 
-
-/** Initialize the backgrounds of the game area
-*/
-SB2.Play.prototype.initBackground = function() {
-    var city, cityNames, i; // Used for temporary setting
-
-    // Set the background color
-    this.game.stage.backgroundColor = SB2.BACKGROUND_COLOR;
-
-    // Add two cities in the background giving a parallax effect
-    this.cities = [];
-    cityNames = ['city1', 'city2'];
-    for ( i = 0; i < cityNames.length; i++) {
-        city = this.game.add.tileSprite(0, 0, 800, 600, cityNames[i]);
-        city.fixedToCamera = true;
-        this.cities.push(city);
-    }
-
-};
-
-
 /** Called when the two players collide
  * @param {Object} c1 The first entity 
  * @param {Object} c2 The second entity */
@@ -280,6 +182,103 @@ SB2.Play.prototype.deathTouch = function (c1, c2) {
     
     deathTween1.start();
     deathTween2.start();
+};
+
+/** Initialize the backgrounds of the game area
+*/
+SB2.Play.prototype.initBackground = function() {
+    var city, cityNames, i; // Used for temporary setting
+
+    // Set the background color
+    this.game.stage.backgroundColor = SB2.BACKGROUND_COLOR;
+
+    // Add two cities in the background giving a parallax effect
+    this.cities = [];
+    cityNames = ['city1', 'city2'];
+    for ( i = 0; i < cityNames.length; i++) {
+        city = this.game.add.tileSprite(0, 0, 800, 600, cityNames[i]);
+        city.fixedToCamera = true;
+        this.cities.push(city);
+    }
+
+};
+
+/** This function initialize the level. (Generate the first platforms) */
+SB2.Play.prototype.initLevel = function () {
+    // For platform creation
+    var ground;
+
+    this.platforms = this.game.add.group(undefined, // Parent group
+			                 'platforms', // Name for debug
+			                 false, // Add directly to the stage
+			                 true, // Enable body
+			                 Phaser.Physics.ARCADE);
+
+    // Here we create the ground.
+    ground = this.platforms.create(0, SB2.HEIGHT - 2*SB2.UNIT, 'plain');
+    //  Scale it to fit the width of the game
+    ground.scale.setTo(SB2.WIDTH*10, 2*SB2.UNIT);
+    //  This stops it from falling away when you jump on it
+    ground.body.immovable = true;
+
+    ground = this.platforms.create(0, SB2.HEIGHT - 4*SB2.UNIT, 'plain');
+    //  Scale it to fit the width of the game
+    ground.scale.setTo(SB2.WIDTH/3, 2*SB2.UNIT);
+    //  This stops it from falling away when you jump on it
+    ground.body.immovable = true;
+
+    ground = this.platforms.create(SB2.WIDTH/2, SB2.HEIGHT - 7*SB2.UNIT, 'plain');
+    //  Scale it to fit the width of the game
+    ground.scale.setTo(SB2.WIDTH/2, 2*SB2.UNIT);
+    //  This stops it from falling away when you jump on it
+    ground.body.immovable = true;
+};
+
+/** Initialize the swap indicators and begin the swap timer. */
+SB2.Play.prototype.initSwap = function () {
+    // For creating indicators
+    var indic;
+
+    this.swapIndicators = this.game.add.group(undefined, 'indicators', true, 
+				    false); // No body
+    this.swapIndicators.alpha = 0;
+
+    // Create 4 bars assembling into a frame
+    // TOP
+    indic = this.swapIndicators.create(0, 0, 'plain');
+    indic.scale.setTo(SB2.WIDTH, SB2.INDIC_THICK);
+    // BOTTOM
+    indic = this.swapIndicators.create(0, SB2.HEIGHT - SB2.INDIC_THICK, 'plain');
+    indic.scale.setTo(SB2.WIDTH, SB2.INDIC_THICK);
+    // LEFT
+    indic = this.swapIndicators.create(SB2.WIDTH - SB2.INDIC_THICK, SB2.INDIC_THICK, 'plain');
+    indic.scale.setTo(SB2.INDIC_THICK, SB2.HEIGHT - 2*SB2.INDIC_THICK);
+    // RIGHT 
+    indic = this.swapIndicators.create(0, SB2.INDIC_THICK, 'plain');
+    indic.scale.setTo(SB2.INDIC_THICK, SB2.HEIGHT - 2*SB2.INDIC_THICK);
+
+    //Init the indicator tweener
+    this.swapTween = {primary: this.game.add.tween(this.swapIndicators),
+		      secondary: this.game.add.tween(this.swapIndicators)};
+    this.swapTween.primary.from({alpha:0}, SB2.INDIC_PERIOD/2);
+    this.swapTween.secondary.from({alpha:0}, SB2.INDIC_PERIOD/4);
+
+    // Init swap timer
+    this.swap = {timer:this.game.time.now,
+                 count:0};
+};
+
+/** Initialize the controls for player 1 and 2 */
+SB2.Play.prototype.initControls = function () {
+    var kb = this.game.input.keyboard;
+    this.controls1 = new SB2.Controls(kb.addKey(Phaser.Keyboard.UP),
+			               null,
+			               kb.addKey(Phaser.Keyboard.RIGHT),
+			               kb.addKey(Phaser.Keyboard.LEFT));
+    this.controls2 = new SB2.Controls(kb.addKey(Phaser.Keyboard.FIVE),
+			               null,
+			               kb.addKey(Phaser.Keyboard.Y),
+			               kb.addKey(Phaser.Keyboard.R));
 };
 
 
