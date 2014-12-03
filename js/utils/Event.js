b78780a62b8b3f307a92479707385d8150245885
Soreine <soreine.plume@gmail.com>
2014-12-03 12:14:15 +0100

    Corrected events

--- js/utils/Event.js
+++ js/utils/Event.js
@@ -1,19 +1,24 @@
 /* global SB2 */
+/* global SB2 */
 /* global Phaser */
 "use strict";
 
 
 /** Initialize an event
-* @param {number} id Identifier of the event, cf SB2.Events
-* @param {object} <optional> data Complementary data to communicate
-*/
-SB2.Event = function(id, data ={}){
-    this.id = id; 
-    this.data = data; 
+ * @param {number} id Identifier of the event, cf SB2.Events
+ * @param {object} <optional> data Complementary data to communicate
+ */
+SB2.Event = function(id, data = {}){
+    this.id = id;
+    for(var key in data){
+        if(key != "id"){
+            this[key] = data[key];
+        }
+    }
 };
 
 /** All events that could be triggered */
 SB2.Events = {};
-["UPDATE", "RESET", "INIT"].forEach(function(elem, id){
-    SB2.Events[a] = id;
+[""].forEach(function(elem, id){
+    SB2.Events[elem] = id;
 });
--- js/utils/EventManager.js
+++ js/utils/EventManager.js
@@ -2,35 +2,31 @@
 /* global Phaser */
 "use strict";
 
-/** This will handle all event and event's handlers/listeneners 
-*/
+/** This will handle all event and event's handlers/listeneners
+ */
 SB2.EventManager = function(){
-    this.schedule = {};
+    this.listeners = {};
 };
 
 SB2.EventManager.prototype = {
-    /* Bind an listener to a specific event 
-    * @param {Event} event The related event
-    * @param {Function} handler The event's listener
-    */
-    on: function(event, handler) {
-        // TODO! Check if handler is a function
-        this.schedule[event.id] =  this.schedule[event.id] || [];
-        this.schedule[event.id].push(handler);
+    /* Bind an listener to a specific event
+     * @param {Event} event The related event
+     * @param {Function} handler The event's listener
+     */
+    on: function(eventId, handler, caller) {
+        this.listeners[eventId] = this.listeners[eventId] || [];
+        this.listeners[eventId].push({
+            handler: handler,
+            caller: caller
+        });
     },
 
     /* Trigger the event to all listener
-    * @param {Event}  event The related event
-    * @param {[Object, ...]} <optional> All arguments to be pass with
-    */
-    askFor: function(event, args = {}) {
-        // !TODO test the whole thing and add the data of the event to the args
-        this.schedule[event.id].forEach(function(handler){
-            handler.apply(
-                arguments.callee.caller, 
-                Array.prototype.slice.call(arguments, 1)
-            );
+     * @param {Event} event The related event
+     */
+    trigger: function(event) {
+        this.listeners[event.id].forEach(function(listener){
+            listener.handler.call(listener.caller, event);
         });
-    },
+    }
 };
-
